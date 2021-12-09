6bafda6d7b53c68779f6b08fb1f9c776a761d662
Lillie <lennon.m.craig@gamil.com>
2021-12-09 16:31:54 -0500

    Add echoing and input checking

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::env;
+use std::process;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
+    let _message_argument = match args.get(1) {
+        Some(msg_arg) => msg_arg,
+        None => {
+          println!("No input! Exiting");
+          std::process::exit(-1);
+        }
+    };
     let message = &args[1];
     println!("{}", message);
 }
